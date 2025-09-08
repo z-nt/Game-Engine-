641a27caa5b8dc414979cfdcd77db67238ae2887
kiannosratian <kiannosratian76@gmail.com>
2025-09-08 08:15:19 +0330

    change the pos of shape

--- app/core/main.js
+++ app/core/main.js
@@ -16,11 +16,46 @@ const change = document.getElementById("change");
 
 change.addEventListener("click",( )=>{
 	circle.style.width=input.value + "px";	
-
 	circle.style.height=input.value + "px"  ;	
+});
 
+function changePos (obj) {
+	let pluspos= 50;
+ const buttons = document.querySelectorAll(".movement-button");
+	if (obj) {
+	buttons.forEach(btn=>{
+		btn.addEventListener("click",()=>{
+			if(btn.id=="up"){
+			    pluspos -= 10;	
+			   obj.style.top = pluspos + "px";
+			}
+			else if (btn.id =="bottom"){
 
-} );
+			    pluspos += 10;	
+			   obj.style.top= pluspos + "px";
+			}
+			else if (btn.id == "left" ) {
+
+			    pluspos -= 10;	
+			   obj.style.left= pluspos + "px";
+			}
+
+			else if (btn.id == "right" ) {
+
+			    pluspos += 10;	
+			   obj.style.left= pluspos + "px";
+			}else {
+			   console.log("there is no shape");
+	
+			}
+		})
+	})
+	}
+	
+}
+
+changePos(circle);
+
 
 
 
